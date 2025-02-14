b09336a71c393a0f433e7a00a975676063fe59ab
Taras Maslych <taras.maslych@envelope.vision>
2025-02-14 14:42:17 -0500

    Added automated code bundling on editor input change with debouncing

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CodeEditor from "./CodeEditor";
 import Preview from "./Preview.tsx";
 import bundle from "../bundler";
@@ -7,6 +7,17 @@ import Resizable from "./Resizable.tsx";
 function CodeCell() {
   const [rawCode, setRawCode] = useState("import 'bulma/css/bulma.css'");
   const [bundledCode, setBundledCode] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const result = await bundle(rawCode);
+      setBundledCode(result);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [rawCode]);
 
   return (
     <Resizable direction={"vertical"}>
