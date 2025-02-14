93c097c263b1ed32ec604d3812bfcd3ebaf26736
Taras Maslych <taras.maslych@envelope.vision>
2025-02-13 23:20:42 -0500

    Implemented an iFrame for safely running/rendering user's input code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 
 function App() {
-  const [esbuildInitialized, setEsbuildInitialized] = useState(false);
-  const [input, setInput] = useState("const App = () => <div>Tinker JS</div>");
-  const [code, setCode] = useState("");
-
-  const initializeEsbuild = async () => {
-    await esbuild.initialize({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm/esbuild.wasm",
-    });
-  };
+  const esbuildInitialized = useRef(false);
+  const iframe = useRef<any>();
+  const [input, setInput] = useState("import 'bulma/css/bulma.css'");
 
   useEffect(() => {
+    const initializeEsbuild = async () => {
+      await esbuild.initialize({
+        worker: true,
+        wasmURL: "https://unpkg.com/esbuild-wasm/esbuild.wasm",
+      });
+    };
+
     initializeEsbuild()
-      .then(() => setEsbuildInitialized(true))
+      .then(() => (esbuildInitialized.current = true))
       .catch(console.error);
   }, []);
 
   const onClick = async () => {
     if (!esbuildInitialized) return;
+
+    iframe.current.srcdoc = html;
 
     const result = await esbuild.build({
       entryPoints: ["index.js"],
@@ -35,8 +37,30 @@ function App() {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html lang="en">
+    <head>
+      <title>Executed Code</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener("message", (e) => {
+          try {
+            eval(e.data)
+          } catch (e) {
+            const root = document.getElementById("root");
+            root.innerHTML = "<div style='color: red;'><h4>Runtime Error</h4>" + e + "</div>";
+            console.error(e);
+          }
+        }, false);
+      </script>
+    </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -44,7 +68,12 @@ function App() {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="Rendered Code"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 }
